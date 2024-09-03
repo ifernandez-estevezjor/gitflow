41b50a33880e29588e697ada593623fc0ab83c08
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-09-03 12:13:59 -0600

    Create crud-enterprises.php

---
+++ crud-enterprises.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>CRUD Enterprises</h1>
+    <p>Subir archivo a rama creada a partir de rama develop</p>
+</body>
+</html>
